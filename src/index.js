65f576e49767aebb9bbde1e0cac2c0483b919f29
Nicolás Fantone <nfantone@gmail.com>
2018-03-11 23:52:06 +0000

    Bundle tachyons using build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './styles/index.css';
+import './index.css';
+import 'tachyons/css/tachyons.css';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
